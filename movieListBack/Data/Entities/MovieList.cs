fca5c1f6c93e4c390cb548e2fc9f8b87ace4c27b
Abhishek Hanumant Patil <patil.abhishek.66333@gmail.com>
2023-11-04 12:22:42 +0530

    Add project files.

---
+++ movieListBack/Data/Entities/MovieList.cs
@@ -0,0 +1,11 @@
+﻿namespace movieListBack.Data.Entities
+{
+    public class MovieList
+    {
+        public int Id { get; set; }
+        public string? movieName { get; set; }
+        public string? actorName { get; set;}
+        public string? actressName { get; set; }
+        public string? posterUrl { get; set; }
+    }
+}
---
+++ movieListBack/Data/testDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movieListBack.Data.Entities;
+
+namespace movieListBack.Data
+{
+    public class testDatabaseContext : DbContext
+    {
+        public testDatabaseContext(DbContextOptions<testDatabaseContext> context) : base(context)
+        {
+
+        }
+        public DbSet<MovieList> Movies { get; set; }
+    }
+}
